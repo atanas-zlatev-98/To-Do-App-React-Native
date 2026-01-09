6bee1e2644e0df51dcc2f8fefa188660ccb511e8
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2026-01-09 21:06:18 +0200

    add new todo to the state

--- App.js
+++ App.js
@@ -4,19 +4,31 @@ import { Button, Text, TextInput, View } from 'react-native';
 export default function App() {
 
   const [text,setText] = useState('');
+  const [todos,setTodos] = useState([]);
 
   const textChangeHandler = (value) =>{
-    setText(value)
+    setText(value);
   } 
 
   const createTodoHandler = () =>{
-    alert(text);
+
+    if(!text){
+      return alert('Misssing Todo Text!');
+    }
+
+    const newTodo = {
+      text,
+      isCompleted: false,
+    }
+
+    setTodos(oldTodos => [...oldTodos, newTodo]);
+    setText('');
   }
 
   return (
     <View style={styles.body}>
 
-      <View style={{marginBottom:20}}>
+      <View>
         <Text style={styles.heading}>Todo List</Text>
       </View>
 
@@ -36,7 +48,8 @@ export default function App() {
 const styles = {
   body: {
     padding:20,
-    alignItems:'center'
+    alignItems:'center',
+    gap:30
   },
   heading:{
     fontSize:30,
