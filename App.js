9aa1758de338a8d5223e28edb7a5ebc2900d0135
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2026-01-09 20:58:19 +0200

    added todo input

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [text,setText] = useState('');
+
+  const textChangeHandler = (value) =>{
+    setText(value)
+  } 
+
+  const createTodoHandler = () =>{
+    alert(text);
+  }
+
   return (
     <View style={styles.body}>
 
-      <View>
-        <Text style={styles.heading}>Heading</Text>
+      <View style={{marginBottom:20}}>
+        <Text style={styles.heading}>Todo List</Text>
       </View>
 
-      <View>
-        <Text>Creation</Text>
+      <View style={{flexDirection:'row',borderWidth:1,justifyContent:'space-between',width:"100%",borderRadius:5}}>
+        <TextInput placeholder='Go to the gym' value={text} onChangeText={textChangeHandler}></TextInput>
+        <Button title="Create" onPress={createTodoHandler}></Button>
       </View>
 
       <View>
@@ -29,5 +42,5 @@ const styles = {
     fontSize:30,
     marginTop:20,
     fontWeight:'bold'
-  }
+  },
 }
